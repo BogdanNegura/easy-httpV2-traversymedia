b03a69d5c89e7626a632471036f3cc57c9ff420a
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-31 22:26:56 +0100

    v3 for "POST" with async, awit

--- easyhttp3.js
+++ easyhttp3.js
@@ -17,19 +17,18 @@
     }
 
     // Make an HTTP POST Request
-    post(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'aplication/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
+    async post(url, data) {
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'aplication/json'
+            },
+            body: JSON.stringify(data)
         });
+        const resData = await response.json();
+        return resData;
+  
     }
 
     //  Make an HTTP PUT Request
